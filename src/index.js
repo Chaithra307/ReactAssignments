f18c5c9f4261ff80517a5196b4507f4db4e42e9f
chaithra307 <akankshachaithra30@gmail.com>
2023-03-02 20:48:16 +0530

    call

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {createRoot} from "react-dom/client"
-import App from './function based component/App'
 
-createRoot(document.getElementById('root')).render(<App/>)
+import Call from './function based component/Call';
+
+createRoot(document.getElementById('root')).render(<Call/>)
