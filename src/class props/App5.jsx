bc0f40c62ad8f5523d1e174696ddf101c78c83a4
chaithra307 <akankshachaithra30@gmail.com>
2023-03-08 19:24:59 +0530

    props

---
+++ src/class props/App5.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Component } from "react";
+import Nav from './Nav5';
+
+class App extends Component
+{
+    render()
+    {
+        return(
+            <Nav data="hello"/>
+        )
+    }
+
+}
+export default App
---
+++ src/function props/App4.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Nav from './Nav4';
+
+
+
+const App=()=>{
+    return(
+        <Nav data="shriya" />
+    )
+
+}
+export default App
