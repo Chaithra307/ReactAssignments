abdc991046ba73a60910acd7b0e0377448efb525
chaithra307 <akankshachaithra30@gmail.com>
2023-03-11 11:55:17 +0530

    boolean props

---
+++ src/App12.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Course from './Course';
+
+const App12 = () => {
+  return (
+    <>
+    <Course courseDetails={{courseName:"Java Full Stack",
+                            courseAvail:true,
+                            courseDuration:"3 months"}}/>
+    
+    </>
+  )
+}
+
+export default App12
