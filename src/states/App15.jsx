7d15bbc2581ce0c044cfbd5fcc21eb4cb0b0948e
chaithra307 <akankshachaithra30@gmail.com>
2023-03-13 20:02:31 +0530

    state object array

---
+++ src/states/App15.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App15 extends Component {
+    state={subject:"REACT JS",
+            skills:["HTML","CSS","JS"]
+           }
+    render() {
+        return (
+            <>
+            <h1>{this.state.subject}</h1>
+            <ul>{this.state.skills.map((x)=>{return<li>{x}</li>})}</ul>
+            </>
+        )
+    }
+}
+
+export default App15;
