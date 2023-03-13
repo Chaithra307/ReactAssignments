021226b7b94dc51db79a943c073333ac22773927
chaithra307 <akankshachaithra30@gmail.com>
2023-03-13 18:50:31 +0530

    state object

---
+++ src/App14.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App14 extends Component {
+    state={username:"SAM"}
+    render() {
+        return (
+            <>
+               <h1>{this.state.username}</h1> 
+            </>
+        );
+    }
+}
+
+export default App14;
